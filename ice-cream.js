3ab5f12216be4c800b4aa9624ca6b5f6e8bfc72b
Jake <jakefantin@gmail.com>
2019-09-07 21:58:40 +0000

    added ice-cream problem and packing-list file

---
+++ ice-cream.js
@@ -0,0 +1,15 @@
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// My code below here:
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+
+// Test 2
+console.log(iceCreamFlavours[0]);
+
+// Test 3
+console.log(iceCreamFlavours[iceCreamFlavours.length - 1]);
+
+//Test 4
+console.log(iceCreamFlavours.length);
